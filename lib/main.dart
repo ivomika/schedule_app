2448213bdea7d1a0d1158f1970360bac493b5f28
ivomika <67991025+ivomika@users.noreply.github.com>
2024-06-12 00:46:05 +0300

    тестовый комит

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:schedule_app/infrastructure/state/schedule_state.dart';
 import 'package:schedule_app/infrastructure/state/settings_state.dart';
